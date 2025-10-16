f3f456ca3534162a8247cc4d9773717895fafc38
RylenRawr <ryanrizkyrahmadisuccess@gmail.com>
2025-10-17 00:39:47 +0800

    biar idup

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,8 +13,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        <link rel="stylesheet" href="{{ asset('build/assets/app-DdRjrGgI.css') }}">
-        <script src="{{ asset('build/assets/app-CA0LuyaK.js') }}" defer></script>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         <!-- Cropper.js -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.12/cropper.min.css">
