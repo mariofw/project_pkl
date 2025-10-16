10678fc8fa9a86467a1580ed2f5a3c09852bc14e
mario <mariofranca1968@gmail.com>
2025-10-16 23:04:03 +0800

    dashboard eror sudah diperbaiki

--- resources/views/admin/services/index.blade.php
+++ resources/views/admin/services/index.blade.php
@@ -1,19 +1,19 @@
 <x-app-layout>
     <x-slot name="header">
-        <div class="flex justify-between items-center">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                {{ __('Manage Services') }}
-            </h2>
-            <a href="{{ route('admin.services.create') }}" class="bg-green-700 hover:bg-green-800 text-white font-bold py-2 px-4 rounded">
-                {{ __('Add New Service') }}
-            </a>
-        </div>
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Manage Services') }}
+        </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+                    <div class="flex justify-end mb-4">
+                        <a href="{{ route('admin.services.create') }}" class="bg-green-700 hover:bg-green-800 text-white font-bold py-2 px-4 rounded">
+                            {{ __('Add New Service') }}
+                        </a>
+                    </div>
                     @if (session('success'))
                         <div class="mb-4 font-medium text-sm text-green-600">
                             {{ session('success') }}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,8 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link rel="stylesheet" href="{{ asset('build/assets/app-DdRjrGgI.css') }}">
+        <script src="{{ asset('build/assets/app-CA0LuyaK.js') }}" defer></script>
 
         <!-- Cropper.js -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.12/cropper.min.css">
